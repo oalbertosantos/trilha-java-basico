4446fd8b03ef001d296830a50baa0ef086e170ba
Alberto Santos <alberto.almcz@outlook.com>
2024-01-30 00:33:14 -0300

    Atualização

---
+++ exercicios-java-basico/src/edu/alberto/primeirasemana/NewOperacoes.java
@@ -0,0 +1,19 @@
+package edu.alberto.primeirasemana;
+
+public class NewOperacoes {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if (condicao1 && condicao2) { //(e) - se as condicoes 1 e 2 forem verdadeiras.
+            System.out.println("As duas condições são verdadeiras! ");
+        }
+
+        if (condicao1 || condicao2) { //(ou) - se a condição 1 ou 2 for verdadeira.
+            System.out.println("Uma das condições é verdadeira! ");
+
+        System.out.println("Fim!");
+        }
+    
+    }
+}
