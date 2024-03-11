0fcea72c2994129053493ca61f85d4baa28f1849
Alberto Santos <alberto.almcz@outlook.com>
2024-03-11 12:28:13 -0300

    Atualização

---
+++ exercicios-java-basico/src/edu/alberto/segundasemana/ResultadoEscolar.java
@@ -0,0 +1,18 @@
+package edu.alberto.segundasemana;
+
+//Condicionais composta - algumas vezes o programa terá que seguir mais de uma jornada de execução condicionado a uma regra de negócio, este cenário é considerando uma estrutura condicional composta ex.:
+
+public class ResultadoEscolar {
+    
+    public static void main(String[] args) {
+        
+        int nota = 6;
+
+        if (nota >= 7)
+        System.out.println("Aprovado!");
+
+        else
+        System.out.println("Reprovado!");
+
+    }
+}
