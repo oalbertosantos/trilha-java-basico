376d7a5891ea6162ba823b42270ed777a36dddd1
Alberto Santos <alberto.almcz@outlook.com>
2024-03-11 11:48:21 -0300

    Atualização

---
+++ exercicios-java-basico/src/edu/alberto/segundasemana/CaixaEletronico.java
@@ -0,0 +1,9 @@
+package edu.alberto.segundasemana;
+
+//Condicionais simples - ocorre uma validação de execução de fluxo somente quando a condição for positiva, considerando uma estrutura simples ex.:
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        
+    }
+}
--- exercicios-java-basico/src/edu/alberto/segundasemana/Operador.java
+++
@@ -1,7 +0,0 @@
-package edu.alberto.segundasemana;
-public class Operador {
-    public static void main(String[] args) {
-        
-
-    }
-}
