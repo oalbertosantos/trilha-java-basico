b400a3937fd966c86b9a67cb7894dda099712119
Alberto Santos <alberto.almcz@outlook.com>
2024-03-15 11:01:40 -0300

    Atualização

---
+++ exercicios-java-basico/src/edu/alberto/segundasemana/ResultadoEscolarNovo.java
@@ -0,0 +1,21 @@
+package edu.alberto.segundasemana;
+
+//Condicionais encadeadas - em um controle de fluxo condicional, nem sempre nos limitamos ao se (if) e ao senão (else), poderemos ter uma terceira ou inumeras condições. Ex.:
+
+public class ResultadoEscolarNovo {
+    
+    public static void main(String[] args) {
+        
+        int nota = 4;
+
+        if (nota >= 7)
+        System.out.println("Aprovado!");
+
+        else if (nota >= 5 && nota < 7)
+        System.out.println("Prova de Recuperação!");
+
+        else
+        System.out.println("Reprovado!");
+
+    }
+}
