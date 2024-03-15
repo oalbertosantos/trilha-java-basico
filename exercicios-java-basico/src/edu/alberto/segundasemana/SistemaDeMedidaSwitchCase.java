92bba1667353c830950daf9993b74adc884995ad
Alberto Santos <alberto.almcz@outlook.com>
2024-03-15 11:49:37 -0300

    Atualização

---
+++ exercicios-java-basico/src/edu/alberto/segundasemana/SistemaDeMedidaSwitchCase.java
@@ -0,0 +1,14 @@
+package edu.alberto.segundasemana;
+
+//Switch Case - compara o valor de cada caso com o da variável sequencialmente, e sempre que encontra um valor correspondente, executa o código associado ao caso. Para evitar que as comparações continuem a ser executadas acrescentamos um BREAK, após um caso correspondente ter sido encontrado, no final de cada bloco de código. O comando BREAK quando executado, encerra a execução da estrutura onde ele se encontra.
+
+public class SistemaDeMedidaSwitchCase {
+    
+    public static void main(String[] args) {
+        
+        String sigla = "M";
+
+        if (sigla == "P")
+            prin
+    }
+}
