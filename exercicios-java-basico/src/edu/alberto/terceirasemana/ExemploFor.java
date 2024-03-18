56ccf5030127cb3615e9df4215b29de32c0a0977
Alberto Santos <alberto.almcz@outlook.com>
2024-03-18 19:15:06 -0300

    Atualização

--- exercicios-java-basico/src/edu/alberto/terceirasemana/ExemploFor.java
+++ exercicios-java-basico/src/edu/alberto/terceirasemana/ExemploFor.java
@@ -1,5 +1,7 @@
 package edu.alberto.terceirasemana;
 
+//O comando FOR permite que uma variável contadora seja testada e incrementada a cada interação, sendo essas informações definidas na chamada do comando. O comando FOR recebe como entrada uma variável contadora, a condição e o valor de incrementação. Ex.:
+
 public class ExemploFor {
 
     public static void main(String[] args) {
